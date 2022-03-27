f20b83db53e411fd32839da7b335f4b273bda0c3
setsumaru1992 <setsumaru@muriyari.work>
2022-03-27 15:22:23 +0900

    first serverless deploy

---
+++ handler.rb
@@ -0,0 +1,11 @@
+require 'json'
+
+def notify_holiday(event:, context:)
+  {
+    statusCode: 200,
+    body: {
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event
+    }.to_json
+  }
+end
